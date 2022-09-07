a04d0c1a467df291ee48ace2c7f60edf186c90e2
LONSID\guohao <guohao@masastack.com>
2022-09-07 11:06:53 +0800

    Initialize

---
+++ Src/Masa.Maui.Plugin.AudioPlayerSample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Masa.Maui.Plugin.AudioPlayerSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ Src/Masa.Maui.Plugin.AudioPlayerSample/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Masa.Maui.Plugin.AudioPlayerSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
